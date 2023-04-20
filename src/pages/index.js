36349e83568655a8ef63ad9eed88932db41c19d4
diego-vecch <josediegoferia@gmail.com>
2023-04-20 17:38:35 -0300

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
+import SearchButton from '@/components/SearchButton'
+import Navbar from '@/components/Navbar'
+
 export default function Home () {
   return (
-    <main> devathon4-team5-frontend </main>
+    <main className='h-screen bg-gradient-to-t to-light-bg1 from-light-bg2 '>
+      <Navbar />
+      <div className=' grid place-items-center  '>
+        <div className='bg-light-form bg-opacity-25 h-96 w-11/12 mt-10'>
+          <input placeholder='Buenos Aires...' className='m-1' />
+          <SearchButton name='Search' />
+        </div>
+      </div>
+    </main>
   )
 }
