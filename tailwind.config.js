933495f8c92488d5b4a1d0d8dc8289f4d4e72109
diego-vecch <josediegoferia@gmail.com>
2023-04-20 17:30:02 -0300

    feat: create color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       }
+    },
+    colors: {
+      light: {
+        lth: '#F2E8FF',
+        bg1: '#EFEBFF',
+        bg2: '#D4C9FF',
+        nav: '#C7B9FF',
+        form: '#9980FF',
+        selec: '#2F20D3',
+        btn: '#260162',
+        rose: '#E10A4B'
+      }
     }
   },
   plugins: []
