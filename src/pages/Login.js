b8b4b1b82271492dde75fdc8bc7593ff124e528a
proyect_icaro <josuevalenzuela87@gmail.com>
2023-04-24 11:57:43 -0400

    feature: add components

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import Icon from '@/components/Icon'
+import Login from '@/components/login/Login'
+import React from 'react'
+
+export default function LoginPage () {
+  return (
+    <div className='h-screen bg-gradient-to-t to-light-bg1 from-light-bg2'>
+      <div className='flex flex-col items-center'>
+        <Icon />
+      </div>
+      <Login />
+    </div>
+  )
+}
