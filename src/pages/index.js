5f56c0c7dc8d98a76eb3cf9c6de55abe71c5c718
diego-vecch <josediegoferia@gmail.com>
2023-04-24 12:25:06 -0300

    fix: update the main page structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,34 @@
 import SearchButton from '@/components/SearchButton'
 import Navbar from '@/components/Navbar'
+import MapRefImg from '@/components/MapRefImg'
 
 export default function Home () {
   return (
     <main className='h-screen bg-gradient-to-t to-light-bg1 from-light-bg2 '>
-      <Navbar />
-      <div className=' grid place-items-center  '>
-        <div className='bg-light-form bg-opacity-25 h-96 w-11/12 mt-10'>
-          <input placeholder='Buenos Aires...' className='m-1' />
-          <SearchButton name='Search' />
-        </div>
+      <Navbar direct='/' />
+      <div className='grid place-items-center'>
+        <section className='bg-light-form bg-opacity-25 h-96 w-11/12 mt-10'>
+          <section className='grid grid-cols-2 m-1'>
+            <div>
+              <input placeholder='Buenos Aires...' />
+              <SearchButton name='Search' />
+            </div>
+            <div> You are currently viewing : Buenos Aires</div>
+          </section>
+          <section className='grid grid-cols-2'>
+            <div>
+              <p>Description provided by markers
+                <ol><li>◾️ Information provided by markers</li>
+                  <li>◾️ Information provided by markers</li>
+                </ol>
+              </p>
+            </div>
+            <div>
+              Interactive map reference image
+              <MapRefImg />
+            </div>
+          </section>
+        </section>
       </div>
     </main>
   )
