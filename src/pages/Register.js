e2d682b8f713804b7fdc91a8686dbaf90bece4c3
proyect_icaro <josuevalenzuela87@gmail.com>
2023-04-25 10:40:11 -0400

    fix: Fix styles in components

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -6,6 +6,7 @@ export default function RegisterPage () {
   return (
     <div className='h-screen bg-gradient-to-t to-light-bg1 from-light-bg2'>
       <div className='flex flex-col items-center mt-12'>
+        {/* Borrar este comentario arriba de Icon App */}
         {/* Icon App */}
         <Icon />
       </div>
