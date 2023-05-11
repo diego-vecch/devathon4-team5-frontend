9207204fa661150278b28771192eaa9679d7ce0c
marcelo <marcelocamusso@hotmail.com>
2023-05-11 11:25:31 -0300

    bugfix:register: Create responsive design.

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,5 +1,25 @@
 import { useState, useCallback } from 'react'
 import Link from 'next/link'
+import * as Yup from 'yup'
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('name is required'),
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().email('The email is not valid').required('email is required'),
+  password: Yup.string().min(6, 'The password must be at least 6 characters').required('password is required')
+})
+
+const validate = (values) => {
+  const errors = {}
+  try {
+    schema.validateSync(values, { abortEarly: false })
+  } catch (err) {
+    err.inner.forEach((e) => {
+      errors[e.path] = e.message
+    })
+  }
+  return errors
+}
 
 export default function Register () {
   const [datos, setDatos] = useState({
@@ -26,17 +46,46 @@ export default function Register () {
       },
       body: JSON.stringify(datos)
     })
-    const content = await url.json()
-    console.log(content)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText)
+        }
+        return response.json()
+      })
+      .then(content => {
+        console.log(content)
+        // Agregar código de validación aquí
+        const errors = validate(datos)
+        if (Object.keys(errors).length === 0) {
+          // Si no hay errores, enviar el formulario
+          <div class='bg-green-100 border border-green-400 text-green-800 px-4 py-3 rounded relative' role='alert'>
+            <strong class='font-bold'>Felicidades!</strong>
+            <span class='block sm:inline'>Su cuenta se ha creado correctamente.</span>
+            <span class='absolute top-0 bottom-0 right-0 px-4 py-3'>
+              <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 5.652a1 1 0 00-1.414 0L10 8.586 6.066 4.652a1 1 0 00-1.414 1.414L8.586 10l-3.934 3.934a1 1 0 101.414 1.414L10 11.414l3.934 3.934a1 1 0 001.414-1.414L11.414 10l3.934-3.934a1 1 0 000-1.414z"/></svg>
+            </span>
+          </div>
+
+          // ...
+        } else {
+          // Si hay errores, mostrarlos al usuario
+          console.log('error')
+          // ...
+        }
+      })
+      .catch((err) => {
+      // Si hay errores, mostrarlos al usuario
+        console.log('error, revisar error.')
+      })
   }, [datos])
 
   return (
-    <div className='fixed flex w-full items-center'>
+    <div className='fixed flex w-full items-center' >
       <div className='relative mx-6 md:mx-auto w-full bg-light-bg2 rounded-lg md:w-1/2 lg:w-1/3 z-20 m-8'>
         <div className='shadow-lg bg-white rounded-lg p-8'>
           <h1 className='text-2xl text-green-400'>Create an account </h1>
-          {/* Register */}
-          <form className='pt-6 pb-2 my-2' method='post' onSubmit={enviarDatos}>
+          {/* Register */} 
+          <form className='pt-6 pb-2 my-2' id='formulario' method='post' onSubmit={enviarDatos}>
             <div className='mb-6'>
               <label className='block text-sm font-bold mb-2' htmlFor='name'>
                 Name
