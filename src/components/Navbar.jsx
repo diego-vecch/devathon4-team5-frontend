5f2cfa0b5338a2cb389593f1d01af6464e2f3c8c
diego-vecch <josediegoferia@gmail.com>
2023-05-27 01:06:47 -0300

    refactor: add profile user to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import Icon from './Icon'
 import Link from 'next/link'
 import useUser from '@/hooks/useUser'
 import dynamic from 'next/dynamic'
+import { useState } from 'react'
 
 function Navbar ({ direct }) {
   const links = [
@@ -12,6 +13,7 @@ function Navbar ({ direct }) {
     { name: 'Contact us', link: '/formContact', id: 3 }
   ]
   const { isLogged, logout } = useUser()
+  const [optionProfile, setOptionProfile] = useState(null)
 
   const handleClick = e => {
     e.preventDefault()
@@ -19,20 +21,35 @@ function Navbar ({ direct }) {
   }
 
   return (
-    <nav className='bg-light-lth bg-opacity-75 h-12 grid grid-cols-3 px-16 pt-2'>
-      <div className='my-3'><Icon /> </div>
-      <ul className='grid grid-cols-3 gap-4 pr-2 py-2 '>
+    <nav className='bg-light-lth bg-opacity-75 h-12 grid grid-cols-3 px-16 pt-4'>
+      <div className='pb-4'><Icon /> </div>
+      <ul className='grid grid-cols-3 gap-4 pr-2 py-1 '>
         {links.map((link) => (
           <li className='hover:text-light-selec text-center' key={link.id}>
             <a href={link.link}>{link.name}</a>
           </li>
         ))}
       </ul>
-      <ul className='grid py-2 pr-16 justify-end'>
-        <li>
-          {isLogged
-            ? <Link className='hover:text-light-selec' href='#' onClick={handleClick}>Logout</Link>
-            : <Link className='hover:text-light-selec' href='/login'>Login</Link>}
+      <ul className='grid pt-1 pr-16 justify-end'>
+        <li><ul>
+          {isLogged &&
+          (
+            <div className='z-10'>
+              <button className='hover:text-light-selec px-2' onClick={() => { setOptionProfile(!optionProfile) }}>
+                name user ▾
+              </button>
+              {optionProfile &&
+                (
+                  <div className='bg-light-lth grid grid-cols-1 z-20 rounded'>
+                    <Link className='hover:text-light-selec px-2 pt-2' href='/profile'>My profile</Link>
+                    <Link className='hover:text-light-selec z-20 bg-light-lth px-2 py-1' href='#' onClick={handleClick}>Logout</Link>
+                  </div>
+                )}
+            </div>
+          )}
+          {!isLogged &&
+          (<Link className='hover:text-light-selec' href='/login'>Login</Link>
+          )}</ul>
         </li>
       </ul>
     </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,9 @@ export default function Home () {
   const [selectPosition, setSelectPosition] = useState(null)
   return (
     <main className='h-screen bg-gradient-to-t to-light-bg1 from-light-bg2 '>
-      <Navbar />
+      <div className='z-10'> <Navbar /></div>
       <div className='grid place-items-center mt-3'>
-        <section className='bg-light-form bg-opacity-25 h-[100%] w-[96%] mt-1 md:max-h-screen'>
+        <section className=' h-[100%] w-[96%] mt-1 md:max-h-screen'>
           <section className='grid grid-cols-3'>
             <div className='p-4'>
               <h2 className='pb-3 pl-2 text-lg text-center animate-pulse-text'>Check the accessibility of places of interest</h2>
@@ -21,7 +21,7 @@ export default function Home () {
               <div className='mt-1'><ButtonUserPosition setSelectPosition={setSelectPosition} /></div>
               <InfoCard selectPosition={selectPosition} />
             </div>
-            <div className='col-span-2 p-4'>
+            <div className='col-span-2 p-4 z-10'>
               <InteractiveMap selectPosition={selectPosition} />
             </div>
           </section>
